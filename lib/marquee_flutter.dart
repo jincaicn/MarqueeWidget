57af07b97e76c1e20b0f87409772ba5faf0cfa0c
djc <703876992@qq.com>
2019-10-17 15:24:50 +0800

    fit icon text widget

--- lib/marquee_flutter.dart
+++ lib/marquee_flutter.dart
@@ -2,100 +2,135 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-
-class MarqueeWidget extends StatefulWidget{
-
+class MarqueeWidget extends StatefulWidget {
+  final List<Widget> children;
   final String text;
   final TextStyle textStyle;
+
   ///滚动方向，水平或者垂直
   final Axis scrollAxis;
+
   ///空白部分占控件的百分比
   final double ratioOfBlankToScreen;
 
   MarqueeWidget({
-    @required this.text,
+    this.children,
+    this.text,
     this.textStyle,
-    this.scrollAxis:Axis.horizontal,
-    this.ratioOfBlankToScreen:0.25,
-  }) :assert(text!=null,);
+    this.scrollAxis: Axis.horizontal,
+    this.ratioOfBlankToScreen: 0.25,
+  });
 
   @override
   State<StatefulWidget> createState() {
-    return new  MarqueeWidgetState();
+    return new MarqueeWidgetState();
   }
 }
 
-class MarqueeWidgetState extends State<MarqueeWidget> with SingleTickerProviderStateMixin{
-
+class MarqueeWidgetState extends State<MarqueeWidget>
+    with SingleTickerProviderStateMixin {
   ScrollController scroController;
   double screenWidth;
   double screenHeight;
-  double position=0.0;
+  double position = 0.0;
   Timer timer;
-  final double _moveDistance=3.0;
-  final int _timerRest=100;
-  GlobalKey _key=GlobalKey();
-
+  final double _moveDistance = 3.0;
+  final int _timerRest = 100;
+  GlobalKey _key = GlobalKey();
 
   @override
   void initState() {
     super.initState();
-    scroController=new ScrollController();
-    WidgetsBinding.instance.addPostFrameCallback((callback){
+    scroController = new ScrollController();
+    WidgetsBinding.instance.addPostFrameCallback((callback) {
       startTimer();
     });
   }
 
-  void startTimer(){
-    double widgetWidth = _key.currentContext.findRenderObject().paintBounds.size.width;
-    double widgetHeight = _key.currentContext.findRenderObject().paintBounds.size.height;
+  void startTimer() {
+    double widgetWidth =
+        _key.currentContext.findRenderObject().paintBounds.size.width;
+    double widgetHeight =
+        _key.currentContext.findRenderObject().paintBounds.size.height;
 
-    timer=Timer.periodic(new Duration(milliseconds: _timerRest), (timer){
-      double maxScrollExtent=scroController.position.maxScrollExtent;
-      double pixels=scroController.position.pixels;
-      if(pixels+_moveDistance>=maxScrollExtent){
-        if(widget.scrollAxis==Axis.horizontal){
+    timer = Timer.periodic(new Duration(milliseconds: _timerRest), (timer) {
+      double maxScrollExtent = scroController.position.maxScrollExtent;
+      double pixels = scroController.position.pixels;
+      if (pixels + _moveDistance >= maxScrollExtent) {
+        if (widget.scrollAxis == Axis.horizontal) {
           //TODO 我也看不懂怎么算的
-          position=(maxScrollExtent-screenWidth*widget.ratioOfBlankToScreen+widgetWidth)/2-widgetWidth+pixels-maxScrollExtent;
-        }else{
-          position=(maxScrollExtent-screenHeight*widget.ratioOfBlankToScreen+widgetHeight)/2-widgetHeight+pixels-maxScrollExtent;
+          position = (maxScrollExtent -
+                      screenWidth * widget.ratioOfBlankToScreen +
+                      widgetWidth) /
+                  2 -
+              widgetWidth +
+              pixels -
+              maxScrollExtent;
+        } else {
+          position = (maxScrollExtent -
+                      screenHeight * widget.ratioOfBlankToScreen +
+                      widgetHeight) /
+                  2 -
+              widgetHeight +
+              pixels -
+              maxScrollExtent;
         }
         scroController.jumpTo(position);
       }
-      position+=_moveDistance;
-      scroController.animateTo(position,duration: new Duration(milliseconds: _timerRest),curve: Curves.linear);
+      position += _moveDistance;
+      scroController.animateTo(position,
+          duration: new Duration(milliseconds: _timerRest),
+          curve: Curves.linear);
     });
   }
-
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    screenWidth=MediaQuery.of(context).size.width;
-    screenHeight=MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
   }
 
-  Widget getBothEndsChild(){
-    if(widget.scrollAxis ==Axis.vertical){
-      String newString=widget.text.split("").join("\n");
+  Widget getBothEndsChild() {
+    if (widget.scrollAxis == Axis.vertical) {
+      if (widget.children.isNotEmpty) {
+        return Center(
+          child: Column(
+            children: widget.children,
+          ),
+        );
+      }
+      String newString = widget.text.split("").join("\n");
       return new Center(
-        child: new Text(newString,style: widget.textStyle,textAlign: TextAlign.center,),
+        child: new Text(
+          newString,
+          style: widget.textStyle,
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
+    if (widget.children.isNotEmpty) {
+      return new Center(
+        child: Row(
+          children: widget.children,
+        ),
       );
     }
     return new Center(
-        child:new Text(widget.text,style: widget.textStyle,)
+      child: new Text(
+        widget?.text ?? '',
+        style: widget.textStyle,
+      ),
     );
   }
 
-  Widget getCenterChild(){
-    if(widget.scrollAxis ==Axis.horizontal){
-      return new Container(width: screenWidth*widget.ratioOfBlankToScreen);
-    }else{
-      return new Container(height: screenHeight*widget.ratioOfBlankToScreen);
+  Widget getCenterChild() {
+    if (widget.scrollAxis == Axis.horizontal) {
+      return new Container(width: screenWidth * widget.ratioOfBlankToScreen);
+    } else {
+      return new Container(height: screenHeight * widget.ratioOfBlankToScreen);
     }
   }
-
-
 
   @override
   void dispose() {
@@ -105,7 +140,7 @@ class MarqueeWidgetState extends State<MarqueeWidget> with SingleTickerProviderS
 
   @override
   Widget build(BuildContext context) {
-    return  new ListView(
+    return new ListView(
       key: _key,
       scrollDirection: widget.scrollAxis,
       controller: scroController,
